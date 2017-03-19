b2fc09ec86900c1049722332855d2c8bc311be9f
Takashi Tamura <tamuratak@users.noreply.github.com>
2017-03-19 11:20:42 +0900

    edit tests

--- test/test_function.rb
+++ test/test_function.rb
@@ -20,19 +20,17 @@ class TestCNTK < Test::Unit::TestCase
 
   def test_function_forward
     v = input_variable([1])
-    f = CNTK.__sin__(v)
+    f = Ops.sin(v)
     r = f.eval({ v => Numo::SFloat[Math::PI/2] })
     assert_equal([1.0], r.data.to_vec )
   end
 
   def test_func_shift_op
-    v0 = NDArrayView.new(DataType_Double, [1], [1.9], DeviceDescriptor.default_device(), true)
     v = input_variable([1], dtype: DataType_Double)
-    f1 = sin(v)
-    f2 = sin()
-    f3 = f1 >> sin()
-    r = f3.eval({v => Value.new(v0)})
-    assert_equal([Math::sin(Math::sin(1.9))],
+    f = sin() >> sin()
+    f = f.(v)
+    r = f.eval({v => Numo::DFloat[1.9]})
+    assert_equal([ Math::sin( Math::sin(1.9) )],
                  r.data.to_vec)
   end
 
